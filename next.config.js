214d9c25a8c7b56f8a38c8619b7a2248a3a3459d
Yousfi Zakaria <zakonaser@gmail.com>
2024-04-12 09:21:07 +0100

    the error was different, check top of next.config.js for correction

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// actually the error was a validation error because we had a env variable (db sqlite), we had to include it in vercel because there is a check done (check src/env.js) that checks wether or not we included the envs that we have in the vercel env variables. 
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
