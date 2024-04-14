0e85120c7e1cd279255bbf80a4ac626cc59c55f9
Yousfi Zakaria <zakonaser@gmail.com>
2024-04-14 19:00:05 +0100

    fixing vercel eslint error

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -54,7 +54,7 @@ export const postRouter = createTRPCRouter({
     
   const author = users.find((user) => user.id === post.authorId);
   
-  if(!author || !author?.username) throw new TRPCError({
+  if(!author?.username) throw new TRPCError({
     code : "INTERNAL_SERVER_ERROR",
     message: "Author for post not found"
   })
