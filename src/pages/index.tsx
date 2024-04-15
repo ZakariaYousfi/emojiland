51eb3160c128b39b591fb659e6da9cd034b8b2f9
Yousfi Zakaria <zakonaser@gmail.com>
2024-04-15 10:03:15 +0100

    loading states, split user loading and posts loading, added loading spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import dayjs
  from "dayjs";
  import relativeTime from "dayjs/plugin/relativeTime"
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
 
  dayjs.extend(relativeTime)
 
@@ -32,13 +33,27 @@ const PostView = (props: PostWithUser) => {
     <span>{post.content}</span></div></div>
 }
 
+const Feed = () => {
+
+  const {data, isLoading: postsLoading } = api.post.getAll.useQuery()
+  
+  if(postsLoading) return <LoadingPage/>
+
+  if(!data) return <div>Something went wrong</div>
+
+  return (<div className="flex flex-col">
+    {data?.map ((fullPost) => (<PostView {...fullPost} key = {fullPost.post.id}/>))}
+  </div>)
+}
 export default function Home() {
 
-  const user = useUser() 
+  const {isLoaded: userLoaded, isSignedIn }= useUser() 
 
-  const {data} = api.post.getAll.useQuery()
+  // start fetching asap
+  api.post.getAll.useQuery()
+  // return empty div if user isn't loaded yet
+  if(!userLoaded) return <div/>
 
-  console.log(user)
   return (
     <>
       <Head>
@@ -49,11 +64,9 @@ export default function Home() {
       <main className="flex justify-center h-screen">
       <div className="w-full  md:max-w-2xl border-x border-slate-400">
       <div className="border-b border-slate-400 p-4">
-      {user.isSignedIn ? <div ><CreatePostWizard/></div> : <div><SignInButton></SignInButton></div>}
+      {isSignedIn ? <div ><CreatePostWizard/></div> : (isSignedIn && (<div className = "flex justify-center">  <SignInButton></SignInButton></div>))}
       </div>
-      <div className="flex flex-col">
-        {data?.map ((fullPost) => (<PostView {...fullPost} key = {fullPost.post.id}/>))}
-      </div>
+      <Feed/>
       </div>
       </main>
     </>
