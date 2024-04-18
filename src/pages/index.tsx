ccfdd62fb50ef68e1cb0438857593a7a06aa5053
Yousfi Zakaria <zakonaser@gmail.com>
2024-04-18 19:08:40 +0100

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,7 @@ const PostView = (props: PostWithUser) => {
   return <div key={post.id} className="flex p-4 border-b border-slate-400 gap-3">
     <Image src={author.profileImageUrl} className="w-14 h-14 rounded-full" alt={`@${author.username}'s profile picture`} width={56} height = {56} />
     <div className = "flex flex-col">
-      <div className ='flex text-slate-300 font-bold gap-1'><span>{`@${author.username}`}</span> <span className='font-thin'>{` · ${dayjs(post.createdAt).fromNow()}`}</span></div>
+      <div className ='flex text-slate-300 font-bold gap-1'><Link href = {`/@${author.username}`}><span>{`@${author.username}`}</span></Link><Link href = {`/post/${post.id}`}><span className='font-thin'>{` · ${dayjs(post.createdAt).fromNow()}`}</span></Link></div>
     <span className = "text-2xl">{post.content}</span></div></div>
 }
 
