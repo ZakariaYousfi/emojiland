17e2c6307f713df11bd719b12bd5b1c2da76c7cf
Yousfi Zakaria <zakonaser@gmail.com>
2024-04-18 21:13:35 +0100

    added static generation to remove load state for profile page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast"
+import Head from "next/head";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -14,6 +15,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
       <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Emojiland</title>
+        <meta name="description" content="Twitter, but emojis only" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
         <Toaster position = "bottom-center"/>
       <Component {...pageProps} />
       </ClerkProvider>
