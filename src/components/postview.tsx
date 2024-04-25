116f7c7e4f199ab3d997bf753befccae82de2ea2
Yousfi Zakaria <zakonaser@gmail.com>
2024-04-25 22:44:20 +0100

    added better link for post

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -14,10 +14,10 @@ type PostWithUser = RouterOutputs["post"]["getAll"][number]
 
 export const PostView = (props: PostWithUser) => {
   const {post, author} = props
-  return <div key={post.id} className="flex p-4 border-b border-slate-400 gap-3">
+  return <Link href = {`/post/${post.id}`}><div key={post.id} className="flex p-4 border-b border-slate-400 gap-3">
     <Image src={author.profileImageUrl} className="w-14 h-14 rounded-full" alt={`@${author.username}'s profile picture`} width={56} height = {56} />
     <div className = "flex flex-col">
-      <div className ='flex text-slate-300 font-bold gap-1'><Link href = {`/@${author.username}`}><span>{`@${author.username}`}</span></Link><Link href = {`/post/${post.id}`}><span className='font-thin'>{` · ${dayjs(post.createdAt).fromNow()}`}</span></Link></div>
-    <span className = "text-2xl">{post.content}</span></div></div>
+      <div className ='flex text-slate-300 font-bold gap-1'><Link href = {`/@${author.username}`}><span>{`@${author.username}`}</span></Link><span className='font-thin'>{` · ${dayjs(post.createdAt).fromNow()}`}</span></div>
+    <span className = "text-2xl">{post.content}</span></div></div></Link>
 }
 
